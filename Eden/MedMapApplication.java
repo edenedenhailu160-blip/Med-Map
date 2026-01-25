17d2ccd6cff3a795a5a432f67c7ed955aae78dca
edenedenhailu160-blip <edenedenhailu160@gmail.com>
2026-01-25 16:39:57 +0300

    Create MedMapApplication.java

---
+++ Eden/MedMapApplication.java
@@ -0,0 +1,11 @@
+package com.medmap.medmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedMapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedMapApplication.class, args);
+    }
+}
